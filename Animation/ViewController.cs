9f3a0572a66c165e2a772dafc96f0feb3de5e173
Thomas Dubiel <td@crazynexus.de>
2017-06-15 15:28:28 +0200

    initial commit

---
+++ Animation/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using UIKit;
+
+namespace Animation
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			newButton.TouchUpInside += NewButton_TouchUpInside;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		void NewButton_TouchUpInside(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
